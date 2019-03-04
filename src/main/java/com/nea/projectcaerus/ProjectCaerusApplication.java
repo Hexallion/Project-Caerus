e62513da90d96159aacbeb9579e6798abddbc3b5
Peter <peter.cresswell7@gmail.com>
2019-03-04 21:32:15 +0000

    Initial Commit

---
+++ src/main/java/com/nea/projectcaerus/ProjectCaerusApplication.java
@@ -0,0 +1,13 @@
+package com.nea.projectcaerus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCaerusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCaerusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nea/projectcaerus/ProjectCaerusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nea.projectcaerus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectCaerusApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
